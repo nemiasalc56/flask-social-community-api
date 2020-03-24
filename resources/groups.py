ed809e9ff33f8f9be849396690aa793f339bb52b
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-03-24 18:47:56 -0500

    groups index route works

--- resources/groups.py
+++ resources/groups.py
@@ -2,6 +2,7 @@
 import models
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
+from flask_login import current_user
 
 
 
@@ -16,8 +17,21 @@ groups = Blueprint('groups', 'groups')
 @groups.route('/', methods=['GET'])
 def group_index():
 
+	# loop up groups that belongs to current logged in user
+	current_user_groups = [model_to_dict(group) for group in current_user.groups]
 
-	return "You hit the group index route"
+	# remove user password before returning the information
+	for group in current_user_groups:
+		group['owner_fk'].pop('password')
+
+	print(current_user_groups)
+
+
+	return jsonify(
+		data=current_user_groups,
+		message=f"Successfuly retrieved {len(current_user_groups)} groups",
+		status=200
+		), 200
 
 
 
@@ -29,19 +43,19 @@ def make_group():
 	payload = request.get_json()
 	print(payload)
 
-	group_member = models.Group.create(
+	new_group = models.Group.create(
 		name = payload['name'],
 		owner_fk = payload['owner_fk']
 		)
 	
 	# remove user password
-	group_member_dict = model_to_dict(group_member)
-	group_member_dict['owner_fk'].pop('password')
+	group_dict = model_to_dict(new_group)
+	group_dict['owner_fk'].pop('password')
 
-	print(group_member_dict)
+	print(group_dict)
 
 	return jsonify(
-		data=group_member_dict,
+		data=group_dict,
 		message="Successfuly craeted group",
 		status=200
 		), 200
