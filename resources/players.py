82933e66a93891b574d1e3a870ff6be9152ffef0
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-03-26 22:12:29 -0500

    player create route works

--- resources/players.py
+++ resources/players.py
@@ -1,6 +1,7 @@
 # import our models and blueprint
 import models
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 
 players = Blueprint('players', 'players')
@@ -8,11 +9,25 @@ players = Blueprint('players', 'players')
 
 
 # create route
-@players.route('/', methods=['POST'])
-def player():
+@players.route('/<group_id>', methods=['POST'])
+def player(group_id):
 
 	# get the information from the request
 	payload = request.get_json()
 	print(payload)
+	print(group_id)
 
-	return "You hit the players create route"
+	video = models.Player.create(
+		name=payload['name'],
+		group_fk=group_id
+		)
+
+	video_dict = model_to_dict(video)
+	# remove the owner's password
+	video_dict['group_fk']['owner_fk'].pop('password')
+
+	return jsonify(
+		data=video_dict,
+		message="Succefully created video",
+		status=200
+		), 200
